dc20a22b9fcbc612285656ccf55b0ec2f7076c5d
uk sung jang <lejlove4u@gmail.com>
2023-01-29 12:06:30 +0900

    * change Scala version to 2.13.8 * Delete comment in build.sbt * Add chapter 9 (For Flasher)

--- chapter9/build.sbt
+++ chapter9/build.sbt
@@ -4,7 +4,7 @@ ThisBuild / scalaVersion := "2.13.8"
 
 lazy val root = (project in file("."))
   .settings(
-    name := "template"
+    name := "chapter9"
   )
 
 scalacOptions ++= Seq(
---
+++ chapter9/src/test/scala/FlasherTest.scala
@@ -0,0 +1,43 @@
+import chisel3._
+import chiseltest._
+import org.scalatest.flatspec.AnyFlatSpec
+
+class FlasherTest extends AnyFlatSpec with ChiselScalatestTester {
+  behavior of "Flasher"
+
+  def testFn[T <: FlasherBase](f: T) = {
+    f.io.start.poke(0.U)
+    f.io.light.expect(0.U)
+    f.clock.step()
+    f.io.light.expect(0.U)
+    f.clock.step()
+    f.io.light.expect(0.U)
+    f.io.start.poke(1.U)
+    f.clock.step()
+    f.io.start.poke(0.U)
+    f.io.light.expect(1.U)
+    f.clock.step(5)
+    f.io.light.expect(1.U)
+    f.clock.step()
+    for (_ <- 0 until 2) {
+      f.io.light.expect(0.U)
+      f.clock.step(3)
+      f.io.light.expect(0.U)
+      f.clock.step()
+      f.io.light.expect(1.U)
+      f.clock.step(5)
+      f.io.light.expect(1.U)
+      f.clock.step()
+      f.io.light.expect(0.U)
+    }
+    f.clock.step()
+    f.io.light.expect(0.U)
+    f.clock.step(5)
+    f.io.light.expect(0.U)
+  }
+
+  it should "pass" in {
+    test(new Flasher).withAnnotations(Seq(WriteVcdAnnotation)) { dut => testFn(dut) }
+    //test(new Flasher2).withAnnotations(Seq(WriteVcdAnnotation)) { dut => testFn(dut) }
+  }
+}
