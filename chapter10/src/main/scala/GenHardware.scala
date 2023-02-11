fd1fc3306cac6ec4b662977a3d193b19d22d71b6
lejlove4u <lejlove4u@gmail.com>
2023-02-11 16:10:37 +0900

    CS-8 GenHardware.scala의 FIXME 수정   - Scala의 pow 함수를 이용하여, 변수 n의 비트 폭 만큼 배열의 크기를 정하도록 수정   - Scala pow외 Chisel Wire, Vec 등등을 활용.

--- chapter10/src/main/scala/GenHardware.scala
+++ chapter10/src/main/scala/GenHardware.scala
@@ -1,10 +1,11 @@
 
 import chisel3._
+import scala.math._
 
-class GenHardware extends Module {
+class GenHardware(sqrInWidth: Int, sqrOutWidth: Int) extends Module {
   val io = IO(new Bundle {
-    val squareIn = Input(UInt(8.W))
-    val squareOut = Output(UInt(8.W))
+    val squareIn = Input(UInt(sqrInWidth.W))
+    val squareOut = Output(UInt(sqrOutWidth.W))
     
     val len = Output(UInt(8.W))
     val data = Output(Vec(12, UInt(8.W)))
@@ -12,10 +13,18 @@ class GenHardware extends Module {
 
   // FIXME: [CS-8] If the variable n is larger than the size of squareROM, an error occurs.
   // Therefore, we need to modify the size of variable n and squareROM to be the same.
+  // The Jira issue CS-8 has been fixed using the scala power function.
+  // The array size of squareROM has been modified to fit the bit width of variable n.
   val n = io.squareIn
-  val squareROM = VecInit(0.U, 1.U, 4.U, 9.U, 16.U, 25.U)
-  val square = squareROM(n)
+  val sizeOfRom = pow(2, sqrInWidth)
+  val squareROM = Wire(Vec(sizeOfRom.toInt, UInt(sqrInWidth.W))); 
 
+  for(cnt <- 0 until sizeOfRom.toInt) {
+    var tempVar = cnt;
+    squareROM(cnt) := tempVar.toInt.asUInt(sqrInWidth.W)
+  }
+
+  val square = squareROM(n)
   io.squareOut := square
 
   val msg = "Hello World!"
@@ -27,5 +36,5 @@ class GenHardware extends Module {
 }
 
 object GenHardware extends App {
-  emitVerilog(new GenHardware(), Array("--target-dir", "generated"))
+  emitVerilog(new GenHardware(4, 8), Array("--target-dir", "generated"))
 }
